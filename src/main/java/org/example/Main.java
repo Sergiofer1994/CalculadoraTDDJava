bc28293fed496917f3b5755c7b356457697600eb
Sergiofer1994 <sergiofer1994@gmail.com>
2026-03-09 17:26:03 +0100

    feat:main created

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,20 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+    public static void main(String[] args) {
+
+        calculator.Calculator calculator = new calculator.Calculator();
+
+        double sum = calculator.add(5, 3);
+        double subtraction = calculator.subtract(10, 5);
+        double multiplication = calculator.multiply(4, 5);
+        double division = calculator.divide(10, 5);
+
+        System.out.println("Addition result: " + sum);
+        System.out.println("Subtraction result: " + subtraction);
+        System.out.println("Multiplication result: " + multiplication);
+        System.out.println("Division result: " + division);
+
     }
 }
